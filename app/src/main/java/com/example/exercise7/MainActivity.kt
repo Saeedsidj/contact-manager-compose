07c453c5217ed2ce290c05bfe93d452ab3ea20a4
saeed.gholami <saeedsidjcr@gmail.com>
2023-08-22 12:33:52 +0330

    exercise7 fixed committed

--- app/src/main/java/com/example/exercise7/MainActivity.kt
+++ app/src/main/java/com/example/exercise7/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,18 +36,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ContactListScreen(contacts: List<Contact>) {
-    LazyColumn {
+    LazyColumn (
+        verticalArrangement =Arrangement.spacedBy(5.dp),
+        contentPadding = PaddingValues(14.dp)
+    ){
         itemsIndexed(contacts) { _, contact ->
-            Column {
+            Column (
+                Modifier.clickable{
+                    Log.d("TAG", "ContactItem: ${contact.name}")
+                }.fillMaxWidth().clip(RoundedCornerShape(12.dp)).border(3.dp,Color.Black,
+                    RoundedCornerShape(12.dp)
+                )
+            ){
                 Text(
                     text = contact.name,
                     fontSize = 20.sp,
                     color = Color.Black,
                     modifier = Modifier
                         .padding(16.dp)
-                        .clickable {
-                            Log.d("TAG", "ContactItem: ${contact.name}")
-                        }
                 )
                 Text(
                     text = contact.phoneNumber,
